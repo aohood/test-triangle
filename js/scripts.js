381839f454ebff65cba5eb7bb28c2ef163e4e6fe
Aya Abu <someone990@yahoo.com>
2019-02-12 01:28:35 -0800

    add the else if statement to select between operations

--- js/scripts.js
+++ js/scripts.js
@@ -5,26 +5,52 @@ $(document).ready(function() {
     var number1= parseFloat($("#num1").val());
     var number2 = parseFloat($("#num2").val());
 
-    // $("#add").select(function(){
-    //   add(number1,number2);
+
+
+
+    if( $("#operation").val() === "adding"){
+      alert("The result of "+($("#add").val())+" " +
+      ($("#num1").val()) +
+      " and "+ ($("#num2").val())+ " is "+add(number1,number2));
+    }
+      else if ($("#operation").val() === "subtracting"){
+        alert("The result of "+($("#sub").val())+" " +
+        ($("#num1").val()) +
+        " and "+ ($("#num2").val())+ " is "+sub(number1,number2));
+      }
+      else if ($("#operation").val() === "multiplying") {
+        alert("The result of "+($("#mul").val())+" " +
+        ($("#num1").val()) +
+        " and "+ ($("#num2").val())+ " is "+multiply(number1,number2));
+      }
+      else if (($("#operation").val() === "dividing")){
+        alert("The result of "+($("#divide").val())+" " +
+        ($("#num1").val()) +
+        " on "+ ($("#num2").val())+ " is "+divide(number1,number2));
+
+      }
+
+
+
+
+
+
+
+
+
+
+
+    // $("#sub").click(function(){
+    //   alert(sub(number1,number2));
     // }); //for the click
-
-
-    alert("The result of "+($("#sub").val())+" " +
-    ($("#num1").val()) +
-    " and "+ ($("#num2").val())+ " is "+sub(number1,number2));
-
-    $("#sub").select(function(){
-      sub(number1,number2);
-    }); //for the click
-
-    $("#mul").select(function(){
-      multiply(number1,number2);
-    }); //for the click
-
-    $("#divide").select(function(){
-      divide(number1,number2);
-    }); //for the click
+    //
+    // $("#mul").select(function(){
+    //   multiply(number1,number2);
+    // }); //for the click
+    //
+    // $("#divide").select(function(){
+    //   divide(number1,number2);
+    // }); //for the click
 
   }); //for the submit
 }); //for the document
