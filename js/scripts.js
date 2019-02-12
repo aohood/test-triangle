695647029f493bed24c48e7c776b6159a16dfb46
Aya Abu <someone990@yahoo.com>
2019-02-12 00:21:57 -0800

    change the interface for the calculater

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,46 @@
 $(document).ready(function() {
-  $("#blanks form").submit(function(event) {
 
-    $(".first-name").text($("#fname").val());
-    $(".Last-name").text($("#lname").val());
-    $(".address").text($("#address").val());
-    $(".menu").text($("#menu").val());
-
-
-    $("#confirmation").show();
+  $("form#cal").submit(function(event) {
     event.preventDefault();
+    var number1= parseFloat($("#num1").val());
+    var number2 = parseFloat($("#num2").val());
+
+    // $("#add").select(function(){
+    //   add(number1,number2);
+    // }); //for the click
+
+
+    alert("The result of "+($("#sub").val())+" " +
+    ($("#num1").val()) +
+    " and "+ ($("#num2").val())+ " is "+sub(number1,number2));
+
+    $("#sub").select(function(){
+      sub(number1,number2);
+    }); //for the click
+
+    $("#mul").select(function(){
+      multiply(number1,number2);
+    }); //for the click
+
+    $("#divide").select(function(){
+      divide(number1,number2);
+    }); //for the click
+
+  }); //for the submit
+}); //for the document
 
 
 
-  });
-});
+
+  var add = function(n,m){
+    return n+m;
+  }
+  var sub = function(n,m){
+    return n-m;
+  }
+  var multiply = function(n,m){
+    return n*m;
+  }
+  var divide = function(n,m){
+    return n/m;
+  }
